45f223a05dfc337cb082e35b56a509fae625e60e
Matvei212 <q841479@gmail.com>
2023-03-04 08:58:07 +0200

    Починаю

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+       </a>`
+  )
+  .join('');
+
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+
+  const target = event.target;
+  if (target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${target.dataset.source}" alt="${target.alt}" />
+  `);
+
+  instance.show();
+});
+
 
 console.log(galleryItems);
+
+
+
+

4fd155fb29f5c54596c62c0967612dd60ad90b94
Matvei212 <q841479@gmail.com>
2023-03-05 19:37:21 +0200

    Переніс код з робочого пк

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,41 +1,100 @@
 import { galleryItems } from './gallery-items.js';
+const galleryContainer = document.querySelector('.gallery');
 
-import { galleryItems } from './gallery-items.js';
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
-const gallery = document.querySelector('.gallery');
+galleryContainer.addEventListener('click', onGalleryContainerClick);
 
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}" />
+          </a>
+        </div>
+      `;
+    })
+    .join('');
+}
 
-const galleryMarkup = galleryItems
-  .map(
-    ({ preview, original, description }) =>
-      `<a class="gallery__item" href="${original}">
-         <img class="gallery__image" src="${preview}" alt="${description}" />
-       </a>`
-  )
-  .join('');
-
-
-gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-
-
-gallery.addEventListener('click', event => {
+function onGalleryContainerClick(event) {
   event.preventDefault();
-
-  const target = event.target;
-  if (target.nodeName !== 'IMG') {
+  const isGalleryImageEl = event.target.classList.contains('gallery__image');
+  if (!isGalleryImageEl) {
     return;
   }
 
+  const sourceUrl = event.target.dataset.source;
+  const description = event.target.alt;
+
   const instance = basicLightbox.create(`
-    <img src="${target.dataset.source}" alt="${target.alt}" />
+    <div class="modal">
+      <img class="modal__image" src="${sourceUrl}" alt="${description}" />
+    </div>
   `);
 
   instance.show();
-});
+
+  document.addEventListener('keydown', onModalCloseKeyDown);
+
+  function onModalCloseKeyDown(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      document.removeEventListener('keydown', onModalCloseKeyDown);
+    }
+  }
+}
 
 
-console.log(galleryItems);
 
 
 
 
+// const galleryContainer = document.querySelector('.gallery');
+// const card = (galleryItemCreate(galleryItems));
+// console.log(card);
+
+// galleryContainer.insertAdjacentHTML("beforeend", card);
+// console.log(galleryContainer);
+
+// galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+// function galleryItemCreate (galleryItems) {
+//  return galleryItems.map(({preview, original, description}) => {
+//  return `<div class="gallery__item">
+//              <a class="gallery__link" href="${original}">
+//              <img
+//                     class="gallery__image"
+//                     src="${preview}"
+//                     data-source="${original}"
+//                     alt="${description}"
+//                 />
+//                 </a>
+//                 </div>`})
+//     .join('');
+    
+// }
+
+// function onGalleryContainerClick(e) {
+//     e.preventDefault();
+    
+//     if (e.target.nodeName !== "IMG") {
+//         return;
+//     }
+
+//     const bigImageUrl = e.target.dataset.source;
+//     console.log(bigImageUrl);
+//     const instance = basicLightbox.create(`
+// 		<img width="1400" height="900" src="${bigImageUrl}">
+// 	`);
+//     instance.show();
+
+//     window.addEventListener('keydown', (e) => {
+//         if (e.code === "Escape") {
+//             instance.close();
+//         }
+//     })
+// }
